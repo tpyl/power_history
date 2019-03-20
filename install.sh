b3550df8f7c15679d0a98061cb69e2ca0fd8e23f
Timo Pylvaenaeinen <timo@percipient.ai>
2019-03-19 23:51:38 -0700

    Updated README.md

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "export HISTIGNORE=\"history -a; eval*\":\$HISTIGNORE" >> ~/.bashrc
 echo "bind '\"\C-e\"':\"\\\"history -a; eval \\$\(${DIR}/search_history.py\) \C-m\\\"\"" >> ~/.bashrc
 
+echo ~/.bashrc updated. CTRL+E shortcut will be available in new 
+echo terminals. 
+
