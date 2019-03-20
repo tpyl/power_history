eff251c1512fa23880965ec96e1b5464fd4ede3e
Timo Pylvaenaeinen <timo@percipient.ai>
2019-03-20 07:30:25 -0700

    Improved history handling

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "export HISTIGNORE=\"history -a; eval*\":\$HISTIGNORE" >> ~/.bashrc
-echo "bind '\"\C-e\"':\"\\\"history -a; eval \\$\(${DIR}/search_history.py \\\$COLUMNS\) \C-m\\\"\"" >> ~/.bashrc
+echo "bind '\"\C-e\"':\"\\\"history -a; CMD=\\$\(${DIR}/search_history.py \\\$COLUMNS\); history -s \\\$CMD; eval \\\$CMD \C-m\\\"\"" >> ~/.bashrc
 
 echo ~/.bashrc updated. CTRL+E shortcut will be available in new 
 echo terminals. 
