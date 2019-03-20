ba609dba7e7dcf8045a4633dc625e804420eeaec
Timo Pylvaenaeinen <timo@percipient.ai>
2019-03-20 08:45:48 -0700

    Made the selected command editable
    By re-inserting the selected command to history and up-arrowing
    into it, we can bring the selected command to be editable in bash.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "export HISTIGNORE=\"history -a; CMD*\":\$HISTIGNORE" >> ~/.bashrc
-echo "bind '\"\C-e\"':\"'history -a; CMD=\\$\(${DIR}/search_history.py \\\$COLUMNS\); history -s \\\"\\\$CMD\\\"; eval \\\"\\\$CMD\\\" \C-m'\"" >> ~/.bashrc
+echo "bind '\"\C-e\"':\"'history -a; CMD=\\$\(${DIR}/search_history.py \\\$COLUMNS\); history -s \\\"\\\$CMD\\\"; \C-m\e[A'\"" >> ~/.bashrc
 
 echo ~/.bashrc updated. CTRL+E shortcut will be available in new 
 echo terminals. 
